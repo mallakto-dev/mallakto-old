e6b9ce83094afb30725abc54db7bcaaffec71da8
dimazawr <xushkuinikx@gmail.com>
2020-11-16 19:21:37 +0200

    Add sitemap, add manifest, add different favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,41 @@ module.exports = {
   siteMetadata: {
     title: `Mallakto`,
     description: `Mallakto - веган продукция в Москве`,
+    siteUrl: 'https://mallakto.ru',
     author: `dimazawr`,
   },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-advanced-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Mallakto`,
+        short_name: `Mallakto`,
+        start_url: `/`,
+        background_color: `#fffdfa`,
+        display: `standalone`,
+        icon: `static/favicon-32x32.png`,
+        icons: [
+          {
+            src: `/static/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/static/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+          {
+            src: `/static/apple-touch-icon.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+        ]
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
